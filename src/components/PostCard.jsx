b08b82909352f2adea1632b955232b2d8c2445c3
Shanto Noor <shantonoor900@gmail.com>
2024-05-30 22:25:27 +0600

    comment post added

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -6,65 +6,111 @@ import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import EditIcon from "@mui/icons-material/Edit";
 import { useNavigate } from "react-router-dom";
-import { Box, Divider, Stack } from "@mui/material";
+import {
+  Box,
+  Button,
+  Divider,
+  Snackbar,
+  Stack,
+  TextField,
+} from "@mui/material";
+import { useState } from "react";
 
 export default function PostCard({ post, cursor = false }) {
   const navigate = useNavigate();
+  const [comment, setComment] = useState("");
+  const [comments, setComments] = useState(post?.comments);
+  const [open, setOpen] = useState(false);
 
   return (
-    <Card
-      sx={{ cursor: cursor ? "pointer" : "auto" }}
-      onClick={() => cursor && navigate(`/post-details/${post.id}`)}
-    >
-      <CardHeader
-        avatar={
-          <Avatar sx={{ bgcolor: "#F08" }} aria-label="recipe">
-            {post.name[0]}
-          </Avatar>
-        }
-        action={
-          <IconButton aria-label="Update Post">
-            <EditIcon />
-          </IconButton>
-        }
-        title={post.name}
-        subheader={post.email}
-      />
-      <CardContent>
-        <Typography variant="body2" color="text.secondary">
-          {post.body}
-        </Typography>
+    <>
+      <Card
+        sx={{ cursor: cursor ? "pointer" : "auto" }}
+        onClick={() => cursor && navigate(`/post-details/${post.id}`)}
+      >
+        <CardHeader
+          avatar={
+            <Avatar sx={{ bgcolor: "#F08" }} aria-label="recipe">
+              {post.name[0]}
+            </Avatar>
+          }
+          action={
+            <IconButton aria-label="Update Post">
+              <EditIcon />
+            </IconButton>
+          }
+          title={post.name}
+          subheader={post.email}
+        />
+        <CardContent>
+          <Typography variant="body2" color="text.secondary">
+            {post.body}
+          </Typography>
 
-        {post?.comments && (
-          <Box sx={{ mt: 4 }}>
-            <Typography variant="h5" component="h5">
-              Comments
-            </Typography>
-            <Divider sx={{ mb: 2, mt: 1 }} />
+          {comments && (
+            <Box sx={{ mt: 4 }}>
+              <Typography variant="h5" component="h5">
+                Comments
+              </Typography>
+              <Divider sx={{ my: 4, mt: 1 }} />
 
-            <Stack direction="column" spacing={2}>
-              {post.comments.map((comment, idx) => (
-                <Box
-                  key={idx}
-                  sx={{
-                    border: "1px solid",
-                    borderRadius: "4px",
-                    padding: 2,
-                    borderColor: "divider",
+              <Box sx={{ display: "flex", flexDirection: "column", mb: 3 }}>
+                <TextField
+                  id="outlined-basic"
+                  label="Add a comment"
+                  variant="outlined"
+                  name="comment"
+                  required
+                  sx={{ mb: 1 }}
+                  value={comment}
+                  onChange={(event) => {
+                    setComment(event.target.value);
+                  }}
+                />
+                <Button
+                  onClick={() => {
+                    setComments([
+                      ...comments,
+                      { body: comment, email: "shantonoor900@gmail.com" },
+                    ]);
+                    setComment("");
+                    setOpen(true);
                   }}
                 >
-                  <Typography variant="p" component="p">
-                    {comment.email}
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    {comment.body}
-                  </Typography>
-                </Box>
-              ))}
-            </Stack>
-          </Box>
-        )}
-      </CardContent>
-    </Card>
+                  Add Comment
+                </Button>
+              </Box>
+
+              <Stack direction="column" spacing={2}>
+                {comments.map((comment, idx) => (
+                  <Box
+                    key={idx}
+                    sx={{
+                      border: "1px solid",
+                      borderRadius: "4px",
+                      padding: 2,
+                      borderColor: "divider",
+                    }}
+                  >
+                    <Typography variant="p" component="p">
+                      {comment.email}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      {comment.body}
+                    </Typography>
+                  </Box>
+                ))}
+              </Stack>
+            </Box>
+          )}
+        </CardContent>
+      </Card>
+      <Snackbar
+        open={open}
+        autoHideDuration={5000}
+        onClose={() => setOpen(false)}
+        message="Comment added successfully"
+      />
+    </>
   );
 }
--- src/pages/PostDetails.jsx
+++ src/pages/PostDetails.jsx
@@ -28,36 +28,34 @@ const PostDetails = () => {
   });
 
   if (isPending)
-  return (
-    <Stack spacing={1}>
-      <Stack direction="row" spacing={1}>
-        <Skeleton variant="circular" width={40} height={40} />
-        <Box sx={{ width: "100%" }}>
-          <Skeleton variant="text" width={"100%"} />
-          <Skeleton variant="text" width={"100%"} />
+    return (
+      <Stack spacing={1}>
+        <Stack direction="row" spacing={1}>
+          <Skeleton variant="circular" width={40} height={40} />
+          <Box sx={{ width: "100%" }}>
+            <Skeleton variant="text" width={"100%"} />
+            <Skeleton variant="text" width={"100%"} />
+          </Box>
+        </Stack>
+        <Skeleton variant="rounded" height={120} />
+        <Box>
+          <Grid container spacing={2} sx={{ mt: 2 }}>
+            {Array.from({ length: 4 }, (_, i) => i + 1).map((post, idx) => (
+              <Grid item xs={12} key={idx}>
+                <Stack spacing={2}>
+                  <Box sx={{ width: "100%" }}>
+                    <Skeleton variant="text" width={"50%"} />
+                    <Skeleton variant="rounded" height={40} />
+                  </Box>
+                </Stack>
+              </Grid>
+            ))}
+          </Grid>
         </Box>
       </Stack>
-      <Skeleton variant="rounded" height={120} />
-      <Box>
-        <Grid container spacing={2} sx={{mt: 2}}>
-          {Array.from({ length: 4 }, (_, i) => i + 1).map((post) => (
-            <Grid item xs={12} key={post.id}>
-              <Stack spacing={2}>
-                <Box sx={{ width: "100%" }}>
-                  <Skeleton variant="text" width={"50%"} />
-                  <Skeleton variant="rounded" height={40} />
-                </Box>
-              </Stack>
-            </Grid>
-          ))}
-        </Grid>
-      </Box>
-    </Stack>
-  );
+    );
 
   if (error) return "An error has occurred: " + error.message;
-
-  console.log(data);
 
   return (
     <>
